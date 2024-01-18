3b45d927e4f0a3b7321e5b26fe96b39b1193debd
nguumiho <minhhoang25498@gmail.com>
2024-01-18 16:44:02 +0700

    modify HelloWorldHoang.java

--- src/main/java/com/java5/asm/controller/HelloWorldHoang.java
+++ src/main/java/com/java5/asm/controller/HelloWorldHoang.java
@@ -1,7 +1,7 @@
 package com.java5.asm.controller;
 
-public class HelloWorlHoang {
+public class HelloWorldHoang {
 	public static void main(String[] args) {
-		System.out.println("hello World Hoang");
+		System.out.println("hello World Hoanggggg");
 	}
 }
